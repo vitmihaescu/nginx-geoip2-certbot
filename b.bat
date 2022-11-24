50e4e44339f0c0757783c1250e6eaca360c7fc50
Vit Mihaescu <vit.mihaescu@gmail.com>
2022-11-24 16:32:10 +0000

    Remove local script

--- b.bat
+++
@@ -1,6 +0,0 @@
-@REM docker stop vit.nginx
-@REM docker rm vit.nginx
-docker build -t vitmihaescu/nginx-geoip2-certbot:latest .
-docker tag vitmihaescu/nginx-geoip2-certbot:latest vitmihaescu/nginx-geoip2-certbot:1.23.2-alpine
-@REM docker push vitmihaescu/nginx-geoip2-certbot
-@REM docker push vitmihaescu/nginx-geoip2-certbot:1.23.2-alpine
